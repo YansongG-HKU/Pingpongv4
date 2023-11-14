d64dcd71b11bb71502dbc9e7848af47a2f322a20
YansongG-HKU <yansongg@connect.hku.hk>
2023-11-14 20:11:13 +0800

    PingpongV4
    This is a simple initial program to complete the game of ping pong, as a basis for intensive learning.

---
+++ RL.py
@@ -0,0 +1,38 @@
+import gymnasium as gym
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import random
+
+# Create environment
+env = gym.make('PongDeterministic-v4', render_mode='human')
+
+# Initialize state
+state = env.reset()
+
+done = False
+n_steps = 0
+
+# Game loop
+while not done:
+    # Here we randomly select an action
+    action = env.action_space.sample()
+
+    # Perform actions and get new states, rewards, and game completion status
+    observation = ()
+    observation = env.step(action)
+
+    # Print the contents of the observation
+    print(f"Observation: {observation}")
+
+    # Render the game screen in 'human' mode
+    env.render()
+
+    n_steps += 1
+
+print(f"Episode finished after {n_steps} steps")
+
+
+
+
+
