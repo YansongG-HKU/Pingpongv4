f1cadb6bde3c344a1b8c3f31d91b76811273bd55
YansongG-HKU <yansongg@connect.hku.hk>
2023-11-16 10:41:24 +0800

    update: 2023.11.16
    This is a very simple intensive learning code that works properly. But please don't plagiarize it, because the model in this code has average performance and it's not my final code.
    
    Please don't plagiarize, if you get caught, it's none of my business.

--- RL4.py
+++ RL4.py
@@ -1,87 +1,137 @@
-import gym
+import gymnasium as gym
 import numpy as np
-import random
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten
-from tensorflow.keras.optimizers import Adam
-from collections import deque
+from tensorflow.keras import layers
+import random
 
-# 预处理游戏帧的函数
-def preprocess_frame(frame):
-    gray = np.mean(frame, axis=2).astype(np.uint8)  # 转换为灰度图
-    gray_expanded = np.expand_dims(gray, axis=-1)  # 增加一个维度
-    resized = tf.image.resize(gray_expanded, [80, 80])  # 调整大小为 80x80
-    return np.array(resized).reshape(80, 80, 1)  # 重塑为 (80, 80, 1)
+# Neural network for Deep Q Learning
+def create_q_model(num_actions):
+    inputs = layers.Input(shape=(210, 160, 3))
 
-# 定义 DQN 代理类
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=2000)
-        self.gamma = 0.95  # 折扣率
-        self.epsilon = 1.0  # 探索率
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.model = self._build_model()
+    # Convolutions on the frames
+    layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
+    layer2 = layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
+    layer3 = layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
 
-    def _build_model(self):
-        model = Sequential()
-        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=self.state_size))
-        model.add(Flatten())
-        model.add(Dense(24, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse', optimizer=Adam(learning_rate=0.001))
-        return model
+    layer4 = layers.Flatten()(layer3)
 
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])
+    layer5 = layers.Dense(512, activation="relu")(layer4)
+    action = layers.Dense(num_actions, activation="linear")(layer5)
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+    return tf.keras.Model(inputs=inputs, outputs=action)
 
-    def replay(self, batch_size):
-        if len(self.memory) < batch_size:
-            return
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-            if not done:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+# Environment setup
+env = gym.make('PongDeterministic-v4', render_mode='human')
+num_actions = env.action_space.n
 
-# 创建和初始化环境
-env = gym.make('PongDeterministic-v4')
-state_size = (80, 80, 1)
-action_size = env.action_space.n
-agent = DQNAgent(state_size, action_size)
+# The first model makes the predictions for Q-values which are used to make an action.
+model = create_q_model(num_actions)
+# Build a target model for the prediction of future rewards.
+# The weights of a target model get updated every step_size steps thus when the loss between the Q-values is calculated
+# the target Q-value is stable.
+model_target = create_q_model(num_actions)
+
+# Experience replay buffers
+action_history = []
+state_history = []
+state_next_history = []
+rewards_history = []
+done_history = []
+episode_reward_history = []
+running_reward = 0
+episode_count = 0
+frame_count = 0
+
+# Number of frames to take random action and observe output
+epsilon_random_frames = 50000
+# Number of frames for exploration
+epsilon_greedy_frames = 1000000.0
+# Maximum replay length
+max_memory_length = 100000
+# Train the model after 4 actions
+update_after_actions = 4
+# How often to update the target network
+update_target_network = 10000
+# Using huber loss for stability
+loss_function = tf.keras.losses.Huber()
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.00025, clipnorm=1.0)
+
+epsilon = 1.0
 batch_size = 32
+gamma = 0.99  # Discount factor for past rewards
 
-# 游戏循环
-state = preprocess_frame(env.reset())
-state = np.reshape(state, [1, *state_size])
-done = False
-n_steps = 0
+while True:  # Run until solved
+    reset_info = env.reset()  # env.reset() now returns a tuple
+    state = np.array(reset_info[0])  # The first element is the state
+    episode_reward = 0
 
-while not done:
-    action = agent.act(state)
-    next_state, reward, done, _ = env.step(action)
-    next_state = preprocess_frame(next_state)
-    next_state = np.reshape(next_state, [1, *state_size])
-    agent.remember(state, action, reward, next_state, done)
-    state = next_state
+    for timestep in range(1, 1000000):
+        frame_count += 1
+        if frame_count < epsilon_random_frames or epsilon > np.random.rand(1)[0]:
+            action = np.random.choice(num_actions)
+        else:
+            state_tensor = tf.convert_to_tensor(state)
+            state_tensor = tf.expand_dims(state_tensor, 0)
+            action_probs = model(state_tensor, training=False)
+            action = tf.argmax(action_probs[0]).numpy()
 
-    if len(agent.memory) > batch_size:
-        agent.replay(batch_size)
+        epsilon -= (1.0 - 0.1) / epsilon_greedy_frames
+        epsilon = max(epsilon, 0.1)
 
-    n_steps += 1
+        state_next, reward, done, truncated, info = env.step(action)
+        state_next = np.array(state_next)
+        episode_reward += reward
 
-print(f"Episode finished after {n_steps} steps")
+        action_history.append(action)
+        state_history.append(state)
+        state_next_history.append(state_next)
+        done_history.append(done)
+        rewards_history.append(reward)
+        state = state_next
+
+        if frame_count % update_after_actions == 0 and len(done_history) > batch_size:
+            indices = np.random.choice(range(len(done_history)), size=batch_size)
+
+            state_sample = np.array([state_history[i] for i in indices])
+            state_next_sample = np.array([state_next_history[i] for i in indices])
+            rewards_sample = [rewards_history[i] for i in indices]
+            action_sample = [action_history[i] for i in indices]
+            done_sample = tf.convert_to_tensor(
+                [float(done_history[i]) for i in indices]
+            )
+
+            future_rewards = model_target.predict(state_next_sample)
+            updated_q_values = rewards_sample + gamma * tf.reduce_max(
+                future_rewards, axis=1
+            )
+
+            updated_q_values = updated_q_values * (1 - done_sample) - done_sample
+
+            masks = tf.one_hot(action_sample, num_actions)
+
+            with tf.GradientTape() as tape:
+                q_values = model(state_sample)
+                q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+                loss = loss_function(updated_q_values, q_action)
+
+            grads = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        if frame_count % update_target_network == 0:
+            model_target.set_weights(model.get_weights())
+
+        if done:
+            break
+
+    episode_reward_history.append(episode_reward)
+    if len(episode_reward_history) > 100:
+        del episode_reward_history[:1]
+
+    running_reward = np.mean(episode_reward_history)
+
+    episode_count += 1
+
+    if running_reward > 40:  # Condition to consider the task solved
+        print("Solved at episode {}!".format(episode_count))
+        break
